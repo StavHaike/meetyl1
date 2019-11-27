6f0bebc031b41443d0c8eb9403ff5897403c4415
stavh21-meet <57598482+stavh21-meet@users.noreply.github.com>
2019-11-27 18:44:41 +0200

    Add files via upload

--- Project.py
+++ Project.py
@@ -4,14 +4,16 @@ Likes = []
 
 
 class User():
-	def __init__(self,name,email,user_name, password, friend_list = []):
+	def __init__(self,name,email,user_name, password, friend_list = [],user_posts = []):
 		self.name = name
 		self.email = email
 		self.user_name = user_name
 		self.password = password
 		self.friend_list = friend_list
+		self.user_posts = user_posts
 		
 		Users.append(User)
+		
 
 	def add_friend(self,f_email):
 		self.friend_list.append(f_email)
@@ -21,7 +23,8 @@ class User():
 	def remove_friend(self,f_email):
 		self.friend_list.remove(f_email)
 	def post(self, text):
-		post1 = Post(text, self.user_name)
+		post1 = Post(text, self.user_name, likes =[],comment_list = [])
+		self.user_posts.append(post1)
 		Posts.append(post1)
 		
 
@@ -32,22 +35,26 @@ class User():
 		print("Email: "+self.email)
 		print("Password: "+self.password)
 		print("Friend: "+str(self.friend_list))
-	"""	print("Posts: "+str(self.posts))"""
+	
 	def send_message(self,f_email, text):
 		print("you just send a message to "+f_email)
 
-	def comment(self,comment_,f_email):
-		print("you just comment on "+ f_email + ' post! Your comment is "' +comment_+'"' )		
 
 class Post():
-	def __init__(self,text,author):
+	def __init__(self,text,author,likes = [],comment_list = []):
 		self.text = text
 		self.author = author
-	def like(self, user_name):
+		self.likes = likes
+		self.comment_list = comment_list
+	def like(self, user_name,):
 		Likes.append(user_name)
-		print("you just liked "+author+ " post!")
-
-
+		self.likes.append(user_name)
+		print(self.likes)
+		print(len(self.likes))
+	def comment(self,comment_,user_name):
+		coment1 = (comment_+ " , from: "+user_name)
+		self.comment_list.append(coment1)
+		print(self.comment_list)
 
 
 user1 = User("Loai","Loai17@meet.mit.edu","lori123","myhiddenpassword123")
@@ -57,6 +64,9 @@ user1.remove_friend("jc")
 user1.post("Hello World")
 user1.get_userInfo()
 user1.send_message("jc@gmail.com", "heyyy")
-user1.comment("Hello","asdfghj")
+user1.user_posts[0].comment("Hello","asdfghj")
+user1.user_posts[0].like("dfjk")
+
+
 
 
